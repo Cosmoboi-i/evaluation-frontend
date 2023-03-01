a70f5e823239cd96e3105995f5481098dacc5a25
Aganya Bajaj <aganyabajaj7@gmail.com>
2023-03-01 18:10:01 +0530

    fix: minor issues (need more fix)

--- src/components/card/__tests__/index.test.jsx
+++ src/components/card/__tests__/index.test.jsx
@@ -2,9 +2,23 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Card from "..";
 
+export const mockCard = {
+  id: 1,
+  name: "abc",
+  description: "abc",
+  venue: "abc",
+  datetime: "abc",
+  timezone: "abc",
+  areSeatsAvailable: false,
+  isRegistered: false,
+  isBookmarked: false,
+  imgUrl: "ad",
+  updateEvent: jest.fn(),
+};
+
 describe("Card", () => {
   it("should render properly", () => {
-    const { asFragment } = render(<Card />);
+    const { asFragment } = render(<Card {...mockCard} />);
     expect(asFragment()).toMatchSnapshot();
   });
 });
